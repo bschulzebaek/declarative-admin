9825974b45bef8d7089513ce04d6b413033a58a0
Benedikt Schulze Baek <benedikt@schulze-baek.de>
2025-05-24 15:01:40 +0200

    add product example

--- src/Resources/app/administration/src/module/index.ts
+++ src/Resources/app/administration/src/module/index.ts
@@ -1,10 +1,12 @@
 import { parse } from 'yaml';
 import PromotionModuleDefinition from './promotion.module.yaml?raw';
 import CustomerModuleDefinition from './customer.module.yaml?raw';
+import ProductModuleDefinition from './product.module.yaml?raw';
 
 const modules = [
     parse(PromotionModuleDefinition),
     parse(CustomerModuleDefinition),
+    parse(ProductModuleDefinition),
 ];
 
 export default modules;
